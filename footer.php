094a2b6b282e2e1755619c141de6d353e95e5549
tahirkhaan <tahirkhan.dream@gmail.com>
2017-08-13 23:57:54 +0500

    change footer file of project

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 
 <div class="row" style="padding-top: 60px;">
 	<center><h2 style="color: black;">A B O U T  U S</h2></center>
-	<p style="color: black; text-align: center; padding: 0px 50px 0px 50px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+	<p style="color: black; text-align: center; padding: 0px 50px 0px 50px; padding-top: 60px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
 
 </div>
 
